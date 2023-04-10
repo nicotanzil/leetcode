c11549fdba373b19c5ce5348551472215b749b87
Clementius Tanzil <clementius.tanzil@gdn-commerce.com>
2023-04-10 14:31:27 +0700

    706. Design HashMap

---
+++ java/706_Design_HashMap/MyHashMap.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class MyHashMap {
+
+    int[] array;
+
+    public MyHashMap() {
+        array = new int[1000001];
+        Arrays.fill(array, -1);
+    }
+
+    public void put(int key, int value) {
+        array[key] = value;
+    }
+
+    public int get(int key) {
+        return array[key];
+    }
+
+    public void remove(int key) {
+        array[key] = -1;
+    }
+}
